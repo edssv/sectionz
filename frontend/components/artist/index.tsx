095053b96ca7adac6f9e48febf17fac27b857264
Eduard Sysoev <syspev.edik@gmail.com>
2023-12-13 10:45:03 +0300

    chore(artist): remove files

--- frontend/components/artist/index.tsx
+++
@@ -1,7 +0,0 @@
-import { ArtistMusic } from './music';
-import { ArtistPopularTracks } from './popular-tracks';
-
-export const Artist = {
-  popularTracks: ArtistPopularTracks,
-  music: ArtistMusic
-};
