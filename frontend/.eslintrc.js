f010f1ca43ab71593166d9e92cae04867b9d533d
Eduard Sysoev <syspev.edik@gmail.com>
2023-12-22 16:15:32 +0300

    style(eslint): change 'no-unsafe-return' rule

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -33,7 +33,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-argument': 'off',
-    '@typescript-eslint/no-unsafe-return': 'warn',
+    '@typescript-eslint/no-unsafe-return': 'off',
     '@typescript-eslint/no-unsafe-call': 'warn',
     '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/restrict-template-expressions': 'off',
